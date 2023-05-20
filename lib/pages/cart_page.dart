1862bc304dd0789fb5fd6ab27d038701702ae3eb
ketan <ketan,waghmare@indexnine,com>
2023-05-21 00:44:41 +0530

    - AddToCart implemented - removeFromCart is implemented - icons added

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_ui_screen/components/coffee_tile.dart';
+import 'package:flutter_demo_ui_screen/models/coffee_shop.dart';
+import 'package:provider/provider.dart';
+
+import '../models/coffee.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -8,10 +13,44 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  //remove item from cart
+  void removeFromCart(Coffee coffee) {
+    Provider.of<CoffeeShop>(context, listen: false).removeItemFromCart(coffee);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Cart'),
+    return Consumer<CoffeeShop>(
+      builder: (context, value, child) => SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              //heading
+              Text(
+                'Your cart',
+                style: TextStyle(fontSize: 20),
+              ),
+
+              //list of cart items
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.userCart.length,
+                  itemBuilder: (context, index) {
+                    Coffee eachCoffee = value.userCart[index];
+                    //return the coffee tile
+                    return CoffeeTile(
+                        coffee: eachCoffee,
+                        onPressed: () => removeFromCart(eachCoffee),
+                        icon: Icon(Icons.delete));
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_ui_screen/components/coffee_tile.dart';
 import 'package:flutter_demo_ui_screen/models/coffee_shop.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_demo_ui_screen/models/coffee.dart';
@@ -11,6 +12,20 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  //add to cart
+  void addToCart(Coffee coffee) {
+    // add to cart
+    Provider.of<CoffeeShop>(context, listen: false).addItemToCart(coffee);
+
+    //let user know successfully added message
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Successfully added to cart'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<CoffeeShop>(
@@ -19,7 +34,7 @@ class _ShopPageState extends State<ShopPage> {
                 padding: const EdgeInsets.all(25.0),
                 child: Column(
                   children: [
-                    Text(
+                    const Text(
                       "How Would you like your coffee?",
                       style: TextStyle(fontSize: 20),
                     ),
@@ -32,13 +47,15 @@ class _ShopPageState extends State<ShopPage> {
                             itemCount: value.coffeeShop.length,
                             itemBuilder: (context, index) {
                               //get individual coffee
-                              Coffee eachCoffee = value.coffeeShop[index] as Coffee;
+                              Coffee eachCoffee = value.coffeeShop[index];
 
                               // return the tile for this coffee
-                              return ListTile(
-                                title: Text(eachCoffee.name),
+                              return CoffeeTile(
+                                coffee: eachCoffee,
+                                onPressed: () => addToCart(eachCoffee),
+                                icon: const Icon(Icons.add),
                               );
-                            }))
+                            })),
                   ],
                 ),
               ),
